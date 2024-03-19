1ba7df669cff1b918d0f9afac499ce473d78455a
nocteln <eliottmieze@gmail.com>
2024-03-19 15:01:38 +0100

    landing page

---
+++ app/(main)/learn/page.tsx
@@ -0,0 +1,4 @@
+const LearnPage = () => {
+  return <div>page</div>;
+};
+export default LearnPage;
---
+++ components/sidebar.tsx
@@ -0,0 +1,5 @@
+export const Sidebar = () => {
+  return (
+    <div className="bg-blue-500 h-full lg:w-[256px] fixed flex ">sidebar</div>
+  );
+};
