b6a14f7c2a262e7561d6d71e2152d745a5c92ff9
nocteln <eliottmieze@gmail.com>
2024-03-20 18:58:05 +0100

    learn page wrappers

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,4 +1,23 @@
+import { FeedWrapper } from "@/components/FeedWrapper";
+import { StickyWrapper } from "@/components/StickyWrapper";
+import { Header } from "./header";
+import { UserProgress } from "@/components/UserProgress";
+
 const LearnPage = () => {
-  return <div>page</div>;
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6 ">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="spanish" />
+      </FeedWrapper>
+    </div>
+  );
 };
 export default LearnPage;
