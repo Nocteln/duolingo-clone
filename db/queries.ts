98a1332379f639331dc4747597f2b8917788dec8
Nocteln <eliottmieze@gmail.com>
2024-03-24 21:42:32 +0100

    lesson header

--- db/queries.ts
+++ db/queries.ts
@@ -56,6 +56,9 @@ export const getUnits = cache(async () => {
 
   const normalizedData = data.map((unit) => {
     const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+      if (lesson.challenges.length === 0)
+        return { ...lesson, completed: false };
+
       if (lesson.challenges.length === 0) {
         return { ...lesson, completed: false };
       }
